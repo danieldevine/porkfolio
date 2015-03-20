c13a686c1c76b28720206f9aef042bb4e878d911
Dan Devine <dan@blackroad.ie>
2015-03-20 11:00:19 +0000

    comments

--- content-slide.php
+++ content-slide.php
@@ -1,4 +1,18 @@
-<?php the_post_thumbnail('massivenail'); ?>
+<?php
+  /**
+  * @Theme Name   :    porkfolio
+  * @file         :    content-portfolio.php
+  * @package      :    porkfolio
+  * @author       :    Dan Devine
+  * @license      :    GPLv3
+  * @filesource   :    wp-content/themes/porkfolio/content-portfolio.php
+  *
+  *this is the content for the individual slides.
+  *
+  */
+?>
+
+<?php the_post_thumbnail('massivenail'); ?> <!--this featured image fills the background of the slide-->
 
 <div class="container scrollable">
   <div class="flex-friend">
